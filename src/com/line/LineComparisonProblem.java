2e9322dc3ffb4b7d144218b2db557dc69e6f8783
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-13 23:40:14 +0530

    UC4 Added : Using Java Compare To Method to Compare two lines

--- src/com/line/LineComparisonProblem.java
+++ src/com/line/LineComparisonProblem.java
@@ -2,9 +2,9 @@ package com.line;
 
 import java.util.Scanner;
 
-class Point {
-	public double x;// co-ordinate x
-	public double y;// co-ordinate y
+class LinePoint {
+	public double x;// co-ordinate x parse values
+	public double y;// co-ordinate y parse values
 
 	public double getX() {
 		return x;
@@ -38,43 +38,42 @@ public class LineComparisonProblem {
 		double lineLength1 = lineLength();
 		double lineLength2 = lineLength();
 		if (lineLength1 == lineLength2) {
-			System.out.println("lineLength1 is equal to lineLength2");
+			System.out.println("line Length1 is equal to line Length2");
 		} else if (lineLength1 > lineLength2) {
-			System.out.println("lineLength1 is greater than lineLength2");
+			System.out.println("line Length1 is greater than line Length2");
 		} else {
-			System.out.println("lineLength2 is greater than lineLength1");
+			System.out.println("line Length2 is greater than line Length1");
 		}
 	}
 
 	public static double lineLength() {
-		Point point = new Point();
+		LinePoint point = new LinePoint();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter the value of x1");
+		System.out.println("Enter the value of x1");
 		point.setX((scanner.nextInt()));
 		double x1 = point.getX();
-		System.out.println("enter the value of x2");
+
+		System.out.println("Enter the value of x2");
 		point.setX((scanner.nextInt()));
 		double x2 = point.getX();
-		System.out.println("enter the value of y1");
+
+		System.out.println("Enter the value of y1");
 		point.setY((scanner.nextInt()));
 		double y1 = point.getY();
-		System.out.println("enter the value of y2");
+
+		System.out.println("Enter the value of y2");
 		point.setY((scanner.nextInt()));
 		double y2 = point.getY();
+
 		double x = Math.pow((x1 - x2), 2);
 		double y = Math.pow((y1 - y2), 2);
 		double length = Math.sqrt(x + y);
 		scanner.close();
-
 		return length;
-
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program using OOPS");
-		equalityCheck();
 		compareTo();
-
 	}
-
 }
