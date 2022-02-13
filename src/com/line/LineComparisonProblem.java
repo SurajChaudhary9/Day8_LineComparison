f853bbf01db6652a359c81be8ac4518b2a0a2c75
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-13 23:16:06 +0530

    UC2 : Check For Equality

--- src/com/line/LineComparisonProblem.java
+++ src/com/line/LineComparisonProblem.java
@@ -5,52 +5,68 @@ import java.util.Scanner;
 class Point {
 	public double x;// co-ordinate x
 	public double y;// co-ordinate y
-	public double length;
+	public static double length;
 
 	public double getX() {
 		return x;
 	}
 
-	public void setX(double x) {
-		this.x = x;
+	public class LineComparisionComputation {
+		static void equalityCheck() {
+			double lineLength1 = lineLength();
+			double lineLength2 = lineLength();
+			if (lineLength1 == lineLength2) {
+				System.out.println("equal");
+			} else {
+				System.out.println("not equal");
+			}
+		}
+
+		public static double lineLength() {
+			Point point = new Point();
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("enter the value of x1");
+			point.setX((scanner.nextInt()));
+
+			double x1 = point.getX();
+			System.out.println("enter the value of x2");
+			point.setX((scanner.nextInt()));
+
+			double x2 = point.getX();
+			System.out.println("enter the value of y1");
+
+			point.setY((scanner.nextInt()));
+			double y1 = point.getY();
+
+			System.out.println("enter the value of y2");
+
+			point.setY((scanner.nextInt()));
+			double y2 = point.getY();
+			double x = Math.pow((x1 - x2), 2);
+			return length;
+		}
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Line Comparison Computation Program using OOPS");
+			equalityCheck();
+
+		}
+
 	}
 
 	public double getY() {
-		return y;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
-	public void setY(double y) {
-		this.y = y;
-	}
-}
-
-public class LineComparisonProblem {
-
-	public static double lineLength() {
-		Point point = new Point();
-		Scanner scanner = new Scanner(System.in);
-
-		point.setX((scanner.nextInt()));
-		double x1 = point.getX();
-		point.setX((scanner.nextInt()));
-		double x2 = point.getX();
-		point.setY((scanner.nextInt()));
-		double y1 = point.getY();
-		point.setY((scanner.nextInt()));
-		double y2 = point.getY();
-		double x = Math.pow((x1 - x2), 2);
-		double y = Math.pow((y1 - y2), 2);
-		double length = Math.sqrt(x + y);
-
-		return length;
+	public void setY(int i) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program using OOPS");
+	public void setX(int i) {
+		// TODO Auto-generated method stub
 
-		double lineLength = lineLength();
-		System.out.println("Length of line is " + lineLength);
 	}
-
 }
